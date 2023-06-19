726ecd901f7f2bfb14af4c9fc854f00d36fe8ba9
Cyrillo46 <yahooisbetter46@gmail.com>
2023-06-18 20:40:46 -0400

    rendered first form

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,4 +1,13 @@
+import "./styles.css";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <form className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input type="text" id="item" />
+      </div>
+    </form>
+  );
 }
 export default App;
