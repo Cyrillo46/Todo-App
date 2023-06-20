3b77481a59108663c1ae1f38cb2f6af199cc23f6
Cyrillo46 <yahooisbetter46@gmail.com>
2023-06-19 20:31:09 -0400

    Added button to form

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -2,12 +2,16 @@ import "./styles.css";
 
 function App() {
   return (
-    <form className="new-item-form">
-      <div className="form-row">
-        <label htmlFor="item">New Item</label>
-        <input type="text" id="item" />
-      </div>
-    </form>
+    <>
+      <h1 className="header">Todo List</h1>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+    </>
   );
 }
 export default App;
