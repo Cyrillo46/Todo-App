bce02d38b1573475b7579d39a4a685639bfac14e
Cyrillo46 <yahooisbetter46@gmail.com>
2023-06-20 10:24:43 -0400

    added list item and delete button

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -3,7 +3,6 @@ import "./styles.css";
 function App() {
   return (
     <>
-      <h1 className="header">Todo List</h1>
       <form className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
@@ -11,6 +10,16 @@ function App() {
         </div>
         <button className="btn">Add</button>
       </form>
+      <h1 className="header">Todo List</h1>
+      <ul>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
     </>
   );
 }
